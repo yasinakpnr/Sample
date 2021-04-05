c3d51f41693d026fa2eef37b91f37181f385985f
yasinakpnr <81962278+yasinakpnr@users.noreply.github.com>
2021-04-05 12:42:52 +0200

    Add files via upload

---
+++ tak1_triangle.java
@@ -0,0 +1,14 @@
+package day02_printing_comments;
+
+public class tak1_triangle {
+
+	public static void main(String[] args) {
+	System.out.println("    *");
+	System.out.println("   * *");
+	System.out.println("  *   *");
+	System.out.println(" *     *");
+	System.out.println("*********");
+
+	}
+
+}
